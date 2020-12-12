83f4eb14cb601526770e9c9e4740caf71dccbdab
Nikita <nvp@industrialinnovationgroup.com>
2020-12-12 16:26:51 +0400

    comment testing. I try add master folder

---
+++ master/squirrel.py
@@ -0,0 +1,9 @@
+def squirrel(N):
+    factorial = 1
+
+    if (N > 0):
+        for i in range(1, N + 1):
+            factorial = factorial*i
+        return int(str(factorial)[0])
+    else:
+       return 0
