ff6adb4d782e9927040ae416581b66ee4193997e
Nikita <nvp@industrialinnovationgroup.com>
2020-12-12 17:03:51 +0400

    Now if N = 0 code returns 1

--- master/squirrel.py
+++ master/squirrel.py
@@ -1,7 +1,7 @@
 def squirrel(N):
     factorial = 1
 
-    if (N > 0):
+    if (N >= 0):
         for i in range(1, N + 1):
             factorial = factorial*i
         return int(str(factorial)[0])
