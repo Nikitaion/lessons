32eba3f6101f5a51f0823f43cdfa7cad29469504
Nikita <nvp@industrialinnovationgroup.com>
2020-12-17 14:54:19 +0400

    problem with same values fixed

--- master/SynchronizingTables.py
+++ master/SynchronizingTables.py
@@ -8,14 +8,21 @@ def SynchronizingTables(N, ids, salary):
     salaryWithIds = [(x,y) for x,y in zip(sortedIds,sortedSalary)]
     addToList = False
     newSalaryList = []
+
     for idsIndex, idsValue in enumerate(ids):
         for salaryIndex, salaryValue in enumerate(salaryWithIds):
+            addToList = False
             for index, value in enumerate(salaryValue):
+                if (value == idsValue):
+                    if value == 67:
+                        print("wtf")
+                    addToList = True
+                    continue
+
                 if (addToList == True):
+                    if value == 67:
+                        print("wtf")
                     newSalaryList.append(value)
                     addToList = False
-                    break
-                if (value == idsValue):
-                    addToList = True
-
+                    
     return newSalaryList
