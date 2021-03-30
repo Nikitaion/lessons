527188e329e611864694e007a095311e5267f1c2
NikitaIon <nikitajohngalt@gmail.com>
2021-03-30 17:39:51 +0400

    small changes

--- master/MaximumDiscount.py
+++ master/MaximumDiscount.py
@@ -3,7 +3,7 @@ def MaximumDiscount(N, price):
 
     price = sorted(price, reverse=True) #reverse - descending sort
 
-    for item in range(N - 1):
+    for item in range(N):
         if (item + 1) % 3 == 0:
             result += price[item]
 
