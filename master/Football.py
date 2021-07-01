ac3f17ba284689e93a5a1675aa7b6cbb257cb371
Nikitaion <52353889+Nikitaion@users.noreply.github.com>
2021-07-01 12:32:23 +0400

    Add files via upload

---
+++ master/Football.py
@@ -0,0 +1,15 @@
+# https://skillsmart.ru/algo/lvl1/oafd.html
+
+def Football(F, N):
+    k = 0
+    if F == sorted(F):
+        return False
+    for i in range(N-1):
+        for j in range(k, N):
+            f = F[:]
+            if i != j:
+                f[i], f[j] = f[j], f[i]
+            if f == sorted(F) or f[::-1] == sorted(F):
+                return True
+        k += 1
+    return False
